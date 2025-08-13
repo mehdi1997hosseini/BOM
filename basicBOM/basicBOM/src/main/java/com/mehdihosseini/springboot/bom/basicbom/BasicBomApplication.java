819211c67a3c54bd84bf540b53544213d0e06581
mehdi hosseini <mehdi1997hosseini@gmail.com>
2025-08-13 09:40:09 +0330

    first commit

---
+++ basicBOM/basicBOM/src/main/java/com/mehdihosseini/springboot/bom/basicbom/BasicBomApplication.java
@@ -0,0 +1,13 @@
+package com.mehdihosseini.springboot.bom.basicbom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicBomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicBomApplication.class, args);
+    }
+
+}
---
+++ basicBOM/basicBOM/src/test/java/com/mehdihosseini/springboot/bom/basicbom/BasicBomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehdihosseini.springboot.bom.basicbom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicBomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
